3aaefce314c98743ff4e8736d18f11be1c573799
Leon T Johnson <johnson.leon.t@gmail.com>
2020-11-15 17:22:53 -0500

    rename/refactor functions for new framework

--- dev/learn.lua
+++ dev/learn.lua
@@ -1,87 +1,124 @@
--- Dev Env
--- 1. Load this script on norns
--- 2. Open matron
--- 3. Reference any globals with `var = require 'var'`
--- 4. Save file (in vscode) and delete line from #3
--- 5. Use `rerun()` in matron to see changes
+-- moments
+--
+-- Given 300 seconds (max)
+-- of audio to play with,
+-- record as much or as little 
+-- into any point in that time.
+--
+-- Manage 4 different moments 
+-- (looping subsamples) of that 
+-- main audio timespan. 
+-- Manipulate the level, pan, 
+-- rate, loop start/stop, and
+-- filter ban for each moment.
+--
+-- E1 - cursor (select moment)
+-- K1 (hold) - toggle moment 
+--
+-- (After the cursor has been 
+-- placed between rows 1-4)
+-- E2 - start
+-- E3 - length
+-- K2 + E2 - level
+-- K3 + E3 - pan
+-- K3 + E2 - rate
+-- K2 + E3 - loop end
+-- K2 + K3 + E1 - record start
+-- K2 + K3 - record/stop record
+--
+-- (After a moment is toggled 
+-- & cursor is at row 5)
+-- E2 - BP filter min freq 
+--   (LP filter if furthest left)
+-- E3 - BP filter bandwidth 
+--   (HP filter if furthest right)
 
--- Reference
--- N/A
+-- Adjust buffer_length and
+-- max_moment in parameters
 
-file_length = 1200
-max_buffer = 300
+buffer_length = 600
+max_moment = 300
 
 cursor = 1
 alt_2 = false
 
 num_to_string = {'one', 'two', 'three', 'four', 'five', 'six'}
 
--- Add parameters for each of the 6 samples of the main file
-function add_samples()
+audio.level_cut(1) -- softcut master level (same as in LEVELS screen)
+audio.level_adc_cut(1) -- adc to softcut input
+
+softcut.buffer_clear()
+
+-- Add parameters for each of the 6 moments of the main file
+function add_moments()
   for i = 1,6 do
-    params:add_separator('Sample '..i)
+    params:add_separator('moment '..i)
 
     params:add{
-      type='number', id='s_'..i..'_start', name='Sample '..i..' Start', 
-      min=0, max=file_length, default=0
+      type='number', id='s_'..i..'_start', name='moment '..i..' Start', 
+      min=0, max=buffer_length, default=0
     }
 
     params:add{
-      type='number', id='s_'..i..'_length', name='Sample '..i..' Length', 
-      min=0, max=max_buffer, default=0
+      type='number', id='s_'..i..'_length', name='moment '..i..' Length', 
+      min=0, max=max_moment, default=0
     }
 
     params:add{
-      type='number', id='s_'..i..'_level', name='Sample '..i..' Level', 
+      type='number', id='s_'..i..'_level', name='moment '..i..' Level', 
       min=-math.huge, max=0, default=0
     }
 
     -- To set an action here for level, use util.dbamp(), as in screen below
 
     params:add{
-      type='number', id='s_'..i..'_pan', name='Sample '..i..' Pan', 
+      type='number', id='s_'..i..'_pan', name='moment '..i..' Pan', 
       min=-1, max=1, default=0
     }
 
     params:add{
-      type='number', id='s_'..i..'_rate', name='Sample '..i..' Rate', 
+      type='number', id='s_'..i..'_rate', name='moment '..i..' Rate', 
       min=-5, max=5, default=1
     }
   end
 end
 
-function init_samples()
+function init_stereo_moments()
   -- send audio input to softcut input
-  audio.level_adc_cut(1)
-  softcut.buffer_clear()
 
+  for i=1,2 do
+    softcut.buffer(1,i)
+  end
+end
+
+function init_moments()
   for i=1,6 do
-    softcut.enable(i,1)
-    softcut.buffer(i,1)
-    softcut.buffer(i,2)
+    
+    softcut.loop_start(i,1)
+    softcut.loop_end(i,6)  -- This seems to work
     softcut.loop(i,1)
-    softcut.loop_start(1,1)
-    softcut.loop_end(1,6)  -- This seems to work
+    softcut.fade_time(i, 0.1) -- voice 1 fade time
     softcut.position(i,1)
     softcut.play(i,0)
     softcut.rate_slew_time(i,1)
-    softcut.level_slew_time(i,1)
+    softcut.level_slew_time(i,0.1)
     softcut.pan_slew_time(i,1)
+    softcut.enable(i,1)
   end
 end
 
 function init()
-  -- Set initial sample values
-  add_samples()
-  init_samples()
+  -- Set initial moment values
+  add_moments()
+  init_moments()
 end
 
-function draw_sample_line(i)
+function draw_moment_line(i)
   line_start = params:get('s_'..i..'_start')
-  line_start = math.floor((line_start / file_length) * 100)
+  line_start = math.floor((line_start / buffer_length) * 100)
 
   line_length = params:get('s_'..i..'_length')
-  line_length = math.floor((line_length / file_length) * 100)
+  line_length = math.floor((line_length / buffer_length) * 100)
 
   line_end = util.clamp(14 + line_start + line_length, 14 + line_start, 114)
   
@@ -106,11 +143,11 @@ function draw_sample_line(i)
   screen.stroke()
 end
 
-function draw_sample_pan(i)
+function draw_moment_pan(i)
   line_pan = params:get('s_'..i..'_pan')
   line_pan = 7 * line_pan
 
-  -- Halfway between the end of the sample line and the edge of the screen is 0 pan
+  -- Halfway between the end of the buffer line and the edge of the screen is 0 pan
   screen.move(121 + line_pan, 5 + 4 * i - 1)
   screen.line(121 + line_pan, 5 + 4 * i + 1)
 end
@@ -122,7 +159,7 @@ function write_param(name, value, move)
   screen.text(value)
 end
 
-function draw_sample_params()
+function draw_moment_params()
   write_param('.-.', num_to_string[cursor], {10, 42})
   
   s_level = params:get('s_'..cursor..'_level')
@@ -143,17 +180,17 @@ function draw_sample_params()
   write_param('.|.', s_pan, {75, 62})
 end
 
--- Draw the line for the sample beneath the main sample line
-function draw_sample(i)
-  draw_sample_pan(i)
-  draw_sample_line(i)
-  draw_sample_params()
+-- Draw the line for the moment beneath the main buffer line
+function draw_moment(i)
+  draw_moment_pan(i)
+  draw_moment_line(i)
+  draw_moment_params()
 end
 
 function redraw()
   screen.clear()
 
-  -- Annotate where the cursor is (referencing sample)
+  -- Annotate where the cursor is (referencing moment)
   screen.pixel(7, 5 + 4 * cursor)
 
   -- Draw audio file line (main line)
@@ -161,16 +198,16 @@ function redraw()
   screen.line(114, 5)
   screen.stroke()
 
-  -- Draw sample line
+  -- Draw moment line
   for i = 1, 6 do
-    draw_sample(i)
+    draw_moment(i)
   end
 
   screen.update()
 end
 
 function enc(n, i)
-  -- Select sample
+  -- Select moment
   if n == 1 then
     cursor = util.clamp(cursor + i, 1, 6)
   end
@@ -236,10 +273,8 @@ function start_recording(i)
   softcut.level(i,1)
   softcut.rate(i,1)
   softcut.position(i,1)
-  softcut.loop_start(i,1)
-  softcut.loop_end(i,5)
   softcut.rec_level(i,1)
-  softcut.pre_level(i,1)
+  softcut.pre_level(i,0.75)
   softcut.rec(i,1)
 end
 
